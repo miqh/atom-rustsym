cfd8f82210ec24d4ae098ef7621fe3848c4c2ddd
Michael Huynh <miqid@outlook.com>
2017-08-16 23:02:36 +0800

    Fix tests broken due to the new Etch-based UI for Atom
    The failures were due to VDOM-related behaviour. It is now necessary to
    allow for a time window in which to wait for elements to be rendered so
    their contents can be tested.

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,33 +1,41 @@
-'use babel';
+/** @babel */
 
 describe('rustsym', () => {
 
-  beforeEach(() => waitsForPromise(() => atom.packages.activatePackage('rustsym')));
+  beforeEach(async () => await atom.packages.activatePackage('rustsym'));
 
   describe('given a Rust source file is open', () => {
 
-    beforeEach(() => waitsForPromise(() => atom.workspace.open('tmp.rs')));
+    beforeEach(async () => await atom.workspace.open('tmp.rs'));
 
-    describe('when the manual file symbols command is used', () => {
-      it('should show the file symbols search view', () => {
+    describe('when the manual file symbols command is used', async () => {
+      it('should show the file symbols search view', async () => {
         let workspaceElement = atom.views.getView(atom.workspace);
         let textEditorElement = atom.views.getView(atom.workspace.getActiveTextEditor());
         jasmine.attachToDOM(textEditorElement);
         atom.commands.dispatch(textEditorElement, 'rustsym:toggle-file-symbols');
         expect(workspaceElement.querySelector('.rustsym')).toExist();
-        let statusBarModeElement = workspaceElement.querySelector('.rustsym-status-bar__mode');
+        let statusBarModeElement;
+        await waitFor(() => {
+          statusBarModeElement = workspaceElement.querySelector('.rustsym-status-bar__mode');
+          return statusBarModeElement != null;
+        });
         expect(statusBarModeElement).toExist();
         expect(statusBarModeElement.innerText.indexOf('file')).not.toBe(-1, 'incorrect search mode');
       });
     });
 
     describe('when the manual project symbols command is used', () => {
-      it('should show the project symbols search view', () => {
+      it('should show the project symbols search view', async () => {
         let workspaceElement = atom.views.getView(atom.workspace);
         jasmine.attachToDOM(workspaceElement);
         atom.commands.dispatch(workspaceElement, 'rustsym:toggle-project-symbols');
         expect(workspaceElement.querySelector('.rustsym')).toExist();
-        let statusBarModeElement = workspaceElement.querySelector('.rustsym-status-bar__mode');
+        let statusBarModeElement;
+        await waitFor(() => {
+          statusBarModeElement = workspaceElement.querySelector('.rustsym-status-bar__mode');
+          return statusBarModeElement != null;
+        });
         expect(statusBarModeElement).toExist();
         expect(statusBarModeElement.innerText.indexOf('project')).not.toBe(-1, 'incorrect search mode');
       });
@@ -36,3 +44,17 @@ describe('rustsym', () => {
   });
 
 });
+
+/**
+ * Simple condition polling helper to support tests involving VDOM behaviour.
+ */
+function waitFor(condition, timeout) {
+  if (typeof timeout === 'undefined') timeout = 3000;
+  const startTime = Date.now();
+  const checkCondition = (resolve, reject) => {
+    if (Date.now() - startTime > timeout) return reject();
+    if (condition()) return resolve();
+    setTimeout(checkCondition, 100, resolve, reject);
+  };
+  return new Promise(checkCondition);
+}
